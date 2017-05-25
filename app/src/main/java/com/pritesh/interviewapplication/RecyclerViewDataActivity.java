3100eeff9a1bb07be15515e0024f5a210c67174c
pritesh.patel <pritam.world@gmail.com>
2017-05-25 15:08:18 -0400

    RecyclerView support added

---
+++ app/src/main/java/com/pritesh/interviewapplication/RecyclerViewDataActivity.java
@@ -0,0 +1,64 @@
+package com.pritesh.interviewapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.pritesh.interviewapplication.adapter.DataRecyclerViewAdapter;
+import com.pritesh.interviewapplication.data.AllData;
+import com.pritesh.interviewapplication.data.DataModel;
+import com.pritesh.interviewapplication.network.ApiClient;
+import com.pritesh.interviewapplication.network.ApiInterface;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RecyclerViewDataActivity extends AppCompatActivity
+{
+
+    RecyclerView mRecyclerView;
+    DataRecyclerViewAdapter mDataRecyclerViewAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_data);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.rvDataList);
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        //llm.setOrientation(LinearLayoutManager.HORIZONTAL);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(llm);
+
+        getRetrofitSupport();
+    }
+
+    private void getRetrofitSupport()
+    {
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<AllData> apiServiceData = apiService.getData();
+        apiServiceData.enqueue(new Callback<AllData>()
+        {
+            @Override
+            public void onResponse(Call<AllData> call, Response<AllData> response)
+            {
+                ArrayList<DataModel>mArrayDataList = response.body().images;
+                mDataRecyclerViewAdapter = new DataRecyclerViewAdapter(RecyclerViewDataActivity.this,mArrayDataList);
+                mRecyclerView.setAdapter(mDataRecyclerViewAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<AllData> call, Throwable t)
+            {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/pritesh/interviewapplication/adapter/DataRecyclerViewAdapter.java
@@ -0,0 +1,72 @@
+package com.pritesh.interviewapplication.adapter;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.pritesh.interviewapplication.R;
+import com.pritesh.interviewapplication.data.DataModel;
+
+import java.util.List;
+
+/**
+ * Created by pritesh.patel on 2017-05-25, 2:53 PM.
+ * ADESA, Canada
+ */
+
+public class DataRecyclerViewAdapter extends RecyclerView.Adapter<DataRecyclerViewAdapter.DataViewHolder>
+{
+    private List<DataModel> mDataModelList;
+    private Activity mActivity;
+
+    public DataRecyclerViewAdapter(Activity activity, List<DataModel> dataModelList)
+    {
+        mActivity = activity;
+        this.mDataModelList = dataModelList;
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return mDataModelList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(DataViewHolder dataViewHolder, int i)
+    {
+        DataModel dm = mDataModelList.get(i);
+        dataViewHolder.mTextView.setText(dm.getText());
+        Glide.with(mActivity).load(dm.getUrl())
+                .thumbnail(1)
+                .into(dataViewHolder.mImageView);
+    }
+
+    @Override
+    public DataViewHolder onCreateViewHolder(ViewGroup viewGroup, int i)
+    {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.item_data_list, viewGroup, false);
+
+        return new DataViewHolder(itemView);
+    }
+
+    static class DataViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView mTextView;
+        ImageView mImageView;
+
+        DataViewHolder(View view)
+        {
+            super(view);
+            mTextView = (TextView) view.findViewById(R.id.txtTitle);
+            mImageView = (ImageView) view.findViewById(R.id.imgData);
+        }
+    }
+}
+
