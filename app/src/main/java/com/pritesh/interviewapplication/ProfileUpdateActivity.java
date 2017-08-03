c2d5606de7eafa0ba050689000509ab41b8141ea
pritesh.patel <pritam.world@gmail.com>
2017-08-03 14:37:40 -0400

    Update profile added

---
+++ app/src/main/java/com/pritesh/interviewapplication/ProfileUpdateActivity.java
@@ -0,0 +1,15 @@
+package com.pritesh.interviewapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfileUpdateActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_update);
+    }
+}
--- app/src/main/java/com/pritesh/interviewapplication/SplashActivity.java
+++ app/src/main/java/com/pritesh/interviewapplication/SplashActivity.java
@@ -19,7 +19,7 @@ public class SplashActivity extends Activity
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashActivity.this, RecipesListActivity.class);
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
             }
